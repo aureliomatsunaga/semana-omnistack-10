72b45878935401515bb21c28721f979d42b2a236
Aurelio Matsunaga <aureliomatsunaga@aurelio-mac.local>
2020-01-17 15:44:20 -0300

    Finished backend

---
+++ backend/src/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const routes = require("./routes");
+
+const app = express();
+
+mongoose.connect(
+  "mongodb+srv://omnistack:naApzvuNfq0xjAUA@cluster0-4c4td.mongodb.net/week10?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
